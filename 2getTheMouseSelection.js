e502d6ab96952ce6be12225b6875177d00cf779d
Mohammad Kowsar <88027531+Kowsar14238@users.noreply.github.com>
2023-11-01 18:44:09 +0600

    Add files via upload

---
+++ 2getTheMouseSelection.js
@@ -0,0 +1,8 @@
+//Get the mouse selection
+//We need to get the content selected by the user.
+const getSelectedText = () =>{
+    window.getSelection().toString();
+}
+getSelectedText();
+
+//Maybe HTML file required
