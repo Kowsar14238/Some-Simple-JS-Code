ccb952755275a397e1cc95fe54714064c7c47c4d
Mohammad Kowsar <88027531+Kowsar14238@users.noreply.github.com>
2023-11-15 23:14:14 +0600

    Add files via upload

---
+++ 19ScrollToPageTop.js
@@ -0,0 +1,7 @@
+//Scroll to the top of the page
+
+const goToTop = () => window.scrollTo(0, 0);
+
+goToTop();
+
+//Maybe HTML file required
---
+++ 20DetermineAppleDevice.js
@@ -0,0 +1,7 @@
+//Determine if it is an Apple device
+
+const isAppleDevice = () => /Mac|iPod|iPhone|iPad/.test(navigator.platform);
+
+isAppleDevice();
+
+//Maybe HTML file required
